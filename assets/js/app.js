5c05de535e9990e71cd965b415b32af23859cbb8
proidp <prodipcv1@gmail.com>
2022-08-03 17:09:12 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,41 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+// let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();//calling the function(optional)
+});
+
+// searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+//   sidebar.classList.toggle("open");
+//   menuBtnChange(); //calling the function(optional)
+// });
+
+// following are the code to change sidebar button(optional)
+function menuBtnChange() {
+ if(sidebar.classList.contains("open")){
+   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+ }else {
+   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+ }
+}
+
+
+$(document).ready( function() {
+  /* Check width on page load*/
+  if ( $(window).width() < 1024) {
+   $('.sidebar').removeClass('open');
+  }
+  else {
+    $('.sidebar').addClass('open');
+  }
+});
+
+$(window).resize(function() {
+  /*If browser resized, check width again */
+  if ($(window).width() < 1024) {
+   $('.sidebar').removeClass('open');
+  }
+  else {$('.sidebar').addClass('open');}
+});
